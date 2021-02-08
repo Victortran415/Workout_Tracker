6f2f59f63fb39da6a0ac9b753bdf6f9b3a172840
Victor Tran <victortran415@gmail.com>
2021-02-08 11:22:42 -0800

    created api for create and update

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-	exercise: [
+	exercises: [
 		{
 			name: {
 				type: String,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,51 @@
-const Workout = require("../models/workoutModel")
+const Workout = require("../models/workoutModel");
+const router = require('express').Router()
 
-module.exports = (app) => {
-    
-    app.get('/api/workout', (req, res) => {
-        Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout)
+//Find all workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
         })
-        .catch(err => {
-            res.status(400).json(err)
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+//create a new workout
+router.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
         })
-    })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
+// Find an existing workout and update by adding another exercise
 
+router.put("/api/workouts/:id", (req, res) => {
+    let exerciseID = req.params.id;
+    Workout.findOneAndUpdate(
+        {
+            _id: exerciseID,
+        },
+        {
+            $push: {
+                exercises: req.body,
+            },
+        },
+        {
+            new: true,
+        }
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
-}
+module.exports = router
