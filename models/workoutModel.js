87c9dca3ff5cefee09b7cec8de36cc0b04dbee4c
Victor Tran <victortran415@gmail.com>
2021-02-08 10:01:21 -0800

    setting up models

---
+++ models/workoutModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+	exercise: [
+		{
+			name: {
+				type: String,
+				trim: true,
+				require: "Enter the exercise you are doing",
+			},
+			type: {
+				type: String,
+				trim: true,
+				require: "Enter what type of exercise you are doing",
+			},
+			weight: {
+				type: Number,
+			},
+			sets: {
+				type: Number,
+			},
+			reps: {
+				type: Number,
+			},
+			duration: {
+				type: Number,
+			},
+		},
+	],
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
